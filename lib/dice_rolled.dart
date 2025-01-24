91343eb7b6e8bc59940cb155a755a585115ccd2f
Vikasini <vikananthu83@gmail.com>
2025-01-24 14:26:56 +0530

    First version of dice-roll app using Flutter

---
+++ lib/dice_rolled.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+class DiceRoller extends StatefulWidget {
+  const DiceRoller({super.key});
+  @override
+  State<DiceRoller> createState() {
+    return _DiceRollerState();
+  }
+}
+
+class _DiceRollerState extends State<DiceRoller> {
+  var currdiceroll = 2;
+  void rolldice() {
+    setState(() {
+      currdiceroll = Random().nextInt(6) + 1;
+    });
+  }
+
+  @override
+  Widget build(context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Image.asset(
+          'assets/images/dice-$currdiceroll.png',
+          width: 200,
+        ),
+        TextButton(
+          onPressed: rolldice,
+          style: TextButton.styleFrom(
+            padding: const EdgeInsets.only(
+              top: 20,
+            ),
+            foregroundColor: Colors.white,
+            textStyle: const TextStyle(
+              fontSize: 28,
+            ),
+          ),
+          child: const Text('Roll Dice'),
+        )
+      ],
+    );
+  }
+}
---
+++ lib/gradient_container.dart
@@ -0,0 +1,25 @@
+import 'package:first_app/dice_rolled.dart';
+import 'package:flutter/material.dart';
+
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer(this.colors, {super.key});
+  final List<Color> colors;
+  
+
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: colors,
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+      child: Center(
+          child: DiceRoller(),
+      ),
+    );
+  }
+}
